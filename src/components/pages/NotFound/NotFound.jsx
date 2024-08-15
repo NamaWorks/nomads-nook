20da8c2d0b7298468cfe23180a80d8c2c147ae31
NamaWorks <pedronayamartinez@hotmail.com>
2024-08-15 19:44:06 +0200

    add 404 page layout

--- src/components/pages/NotFound/NotFound.jsx
+++ src/components/pages/NotFound/NotFound.jsx
@@ -1,21 +1,27 @@
-import React from 'react'
-import NavBar from '../../elements/NavBar/NavBar'
+import './NotFound.css'
 import { useNavScroll } from '../../../utils/hooks/useNavScroll'
+import { Link } from 'react-router-dom'
+
+
 
 const NotFound = () => {
   const { titleSize, navPos, navGap, navHeight, navTop, titleBottom } = useNavScroll(false)
   return (
     
     <>
-    <NavBar
-        titleSize={titleSize}
-        navPos={navPos}
-        navGap={navGap}
-        navHeight={navHeight}
-        navTop={navTop}
-        titleBottom={titleBottom}
-       />
-    <div>404</div>
+    <div className='main-not-found-container'>
+    <div className='not-found-msg-container'>
+      <div className='not-found-title-container'>
+        <p>Error Message</p>
+      </div>
+      <p className='not-found-p'>seems like the page you're looking for could not be found. Let's take you back to the main page.</p>
+      <div className='not-found-dotted-line'></div>
+      <button 
+        className='not-found-back-home-btn'>
+        <Link to="/"><p>back to home</p></Link>
+      </button>
+    </div>
+    </div>
     </>
   )
 }
